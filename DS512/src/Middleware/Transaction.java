8b34c37411e84b9dfdf5be01589e0674b24e8777
AlexandreK88 <a.kishka88@gmail.com>
2013-11-05 16:14:13 -0500

    Created TM, incomplete, untested.

---
+++ DS512/src/Middleware/Transaction.java
@@ -0,0 +1,44 @@
+package Middleware;
+
+import java.util.LinkedList;
+
+import ResInterface.*;
+
+
+
+
+public class Transaction {
+	int id;
+	LinkedList<String> writeRequests;
+	LinkedList<ResourceManager> rmList;
+	
+	public Transaction(int i) {
+		id = i;
+		writeRequests = new LinkedList<String>();
+		rmList = new LinkedList<ResourceManager>();
+	}
+	
+	public int getID() {
+		return id;
+	}
+	
+	public void addWR(String request) {
+		//addrmL(rmL);
+		writeRequests.add(request);
+	}
+	
+	public void addrm(ResInterface.ResourceManager rm) {
+		if (!rmList.contains(rm)) {
+			rmList.add(rm);
+		}
+	}
+	
+	public LinkedList<String> getWR() {
+		LinkedList<String> wrCopy = new LinkedList<String>();
+		for (String s: writeRequests) {
+			wrCopy.add(s);
+		}
+		return wrCopy;
+	}
+
+}
